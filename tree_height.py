0fa0dda99af27bb59574cedd1b45251f6e720e37
Linass <dizzneyk@gmail.com>
2023-03-05 23:12:37 +0200

    Try 1

--- tree_height.py
+++ tree_height.py
@@ -7,9 +7,19 @@ import numpy
 
 def compute_height(n, parents):
     # Write this function
-    max_height = 0
+    max_height = [0] * n
     # Your code here
-    return max_height
+    for i in range(n):
+        node = i
+        h = 0
+        while node != -1:
+            if max_height[node] != 0:
+                h += max_height[node]
+                break
+            h += 1
+            node = parents[node]
+        max_height[i] = h
+    return max(max_height)
 
 
 def main():
@@ -25,18 +35,22 @@ def main():
         for n in fileName:
             if n == 'a':
                 print("[Err]: \tForbidden name")
+        file = open(fileName, "r")
+        nodeCount = int(file.readline().strip())
+        nodes = list(map(int, file.readline().split()))
+        print(compute_height(nodeCount, nodes))
+
     elif textInput == "I":
         print("[!] \tEnter text below.")
-        textInput = input(">:: \t").strip()
+        # input number of elements
+        nodeCount = int(input(">:: \t").strip())
+        # input values in one variable, separate with space, split these values in an array
+        nodes = list(map(int, input(">:: ").split()))
+        # call the function and output it's result
+        print(compute_height(nodeCount, nodes))
+
     else:
         print("[Err] \tWrong choice input")
-
-
-    
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
     pass
 
 # In Python, the default limit on recursion depth is rather low,
